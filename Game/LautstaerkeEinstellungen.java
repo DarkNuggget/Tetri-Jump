e35d7cc988170d3c76c4419e51cdaff7bf8f44ec
DarkNuggget <luk_duemer@web.de>
2025-01-17 18:46:51 +0100

    Add files via upload

---
+++ Game/LautstaerkeEinstellungen.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.Properties;
+
+public class LautstaerkeEinstellungen {
+    private static final String SETTINGS_FILE = "audioSettings.properties";
+
+    // Speichert die Lautstärke in einer Datei
+    public static void saveVolumeSetting(double volume) {
+        Properties properties = new Properties();
+        properties.setProperty("volume", String.valueOf(volume));
+
+        try (FileOutputStream output = new FileOutputStream(SETTINGS_FILE)) {
+            properties.store(output, "Audio Settings");
+            System.out.println("Lautstärke gespeichert: " + volume);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Lädt die Lautstärke aus der Datei
+    public static double loadVolumeSetting() {
+        Properties properties = new Properties();
+        try (FileInputStream input = new FileInputStream(SETTINGS_FILE)) {
+            properties.load(input);
+            String volumeStr = properties.getProperty("volume", "0.5");
+            return Double.parseDouble(volumeStr);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0.5; // Standardwert
+        }
+    }
+}
