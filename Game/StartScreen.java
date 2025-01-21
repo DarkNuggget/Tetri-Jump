ec92e3c5c8b31d5766d644de6c3d9e48463a43e6
DarkNuggget <luk_duemer@web.de>
2025-01-21 09:43:52 +0100

    Update StartScreen.java

--- Game/StartScreen.java
+++ Game/StartScreen.java
@@ -14,30 +14,26 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.geometry.Pos;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import java.io.File;
 import javafx.util.Duration;
 
 public class StartScreen {
     private TetriGui app;
     private Scene scene;
-    private MusikPlayer musikPlayer;
+  
 
     public StartScreen(TetriGui app) {
         this.app = app;
-        this.musikPlayer = new MusikPlayer(); // MusikPlayer instanziieren
         createUI();
     }
 
     public void createUI() {
-        musikPlayer.startMenuMusik();
 
         VBox root = new VBox(20);
         root.setAlignment(Pos.CENTER);
 
         // Hintergrundbild laden
-        File backgroundFile = new File("Hintergrund.jpg");
+        File backgroundFile = new File("Hintergrund/MainMenuHintergrund.jpg");
         String bgUri = backgroundFile.toURI().toString();
         BackgroundImage bgImage = new BackgroundImage(new javafx.scene.image.Image(bgUri),
                 BackgroundRepeat.NO_REPEAT,
@@ -61,14 +57,8 @@ public class StartScreen {
         Button exitButton = new Button("Exit");
         exitButton.setStyle("-fx-font-size: 18px; -fx-padding: 10px 20px;");
         exitButton.setOnAction(event -> System.exit(0));
-
-        // Options-Button
-        Button optionsButton = new Button("Options");
-        optionsButton.setStyle("-fx-font-size: 18px; -fx-padding: 10px 20px;");
-        optionsButton.setOnAction(event -> musikPlayer.showOptionsWindow());
-
         // Buttons zur VBox hinzufügen
-        root.getChildren().addAll(title, startButton, optionsButton, exitButton);
+        root.getChildren().addAll(title, startButton, exitButton);
 
         // Szene erstellen
         scene = new Scene(root, 596, 672);
@@ -107,8 +97,7 @@ public class StartScreen {
                 modeStage.close();
                 app.startGameWithMode(mode); // Spiel im ausgewählten Modus starten
             }
-            musikPlayer.stopMusik();
-            musikPlayer.startGameMusik();
+           
         });
 
         modeRoot.getChildren().addAll(modeTitle, classicTetris, jumpTetris, startModeButton);
