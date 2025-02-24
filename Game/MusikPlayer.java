00dc8a34d041b78c8197a98f204990fca108475d
Dark Nugget <luk_duemer@web.de>
2025-02-24 12:58:37 +0100

    Update MusikPlayer.java

--- Game/MusikPlayer.java
+++ Game/MusikPlayer.java
@@ -15,15 +15,13 @@ import javafx.stage.Stage;
 public class MusikPlayer {
     private MediaPlayer mediaPlayer;
     private List<String> gameMusikListe;
-    private List<String> menuMusikListe; // Deklariere die Liste für die Menü-Musik
+    private List<String> menuMusikListe;
     private final String gameMusik = "Musik/GameMusik/";
     private double aktuelleLautstaerke = 0.5;  // Standardlautstärke
-    private TetriJump tetriJump;
 
     public MusikPlayer() {
-        mediaPlayer = null; // Initialisiere mediaPlayer
-        tetriJump = null;   // Initialisiere tetriJump
-        menuMusikListe = new ArrayList<>(); // Initialisiere die Menü-Musik-Liste
+        mediaPlayer = null;
+        menuMusikListe = new ArrayList<>();
         musikArray(); // Beispiel für das Befüllen der Liste
     }
 
@@ -37,7 +35,6 @@ public class MusikPlayer {
     }
 
     public void musikArray() {
-        // Füge Musikstücke zur Spielmusikliste hinzu
         gameMusikListe = new ArrayList<>();
         gameMusikListe.add("GameMusik1.mp3");
         gameMusikListe.add("GameMusik2.mp3");
@@ -46,83 +43,57 @@ public class MusikPlayer {
         gameMusikListe.add("GameMusik5.mp3");
         gameMusikListe.add("GameMusik6.mp3");
         gameMusikListe.add("GameMusik7.mp3");
-        
-        // Füge Musikstücke zur Menü-Musikliste hinzu
-//        menuMusikListe.add("MenuMusik1.mp3");
+
         menuMusikListe.add("MenuMusik2.mp3");
-//        menuMusikListe.add("MenuMusik3.mp3");
     }
 
-    // Startet die Menü-Musik (zufällig aus der Menü-Liste)
-    public void startMenuMusik() {
-        stopMusik();  // Stoppe zuerst die aktuelle Musik, wenn sie läuft
-        
-        String ausgewählteMusik = waehleZufaelligeMusik(menuMusikListe); // Verwendet menuMusikListe
-        File musicFile = new File("Musik/MenuMusik/" + ausgewählteMusik);
-        speichereMusik("Musik/MenuMusik/", ausgewählteMusik);
-        
-        if (musicFile.exists()) {
-            // Wenn bereits ein MediaPlayer existiert, stoppen und zurücksetzen
-            if (mediaPlayer != null) {
-                mediaPlayer.stop();
-                mediaPlayer.dispose();  // MediaPlayer korrekt freigeben
-            }
-            
-            // Erstelle einen neuen MediaPlayer
-            Media media = new Media(musicFile.toURI().toString());
-            mediaPlayer = new MediaPlayer(media);
-            mediaPlayer.setVolume(this.getAktuelleLautstaerke());  // Setze die Lautstärke
-            mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);    // Musik in Schleife abspielen
-            mediaPlayer.play();
-            // System.out.println("Musikdatei " + musicFile.getName() + " wurde gespielt.");
-        } else {
-            System.out.println("Musikdatei " + musicFile.getName() + " wurde nicht gefunden.");
-        }
-    }
-
-    // Startet die Spiel-Musik (zufällig aus der Spiel-Liste)
-    public void startGameMusik() {
-        stopMusik();  // Stoppe zuerst die aktuelle Musik, wenn sie läuft
-        
-        String ausgewählteMusik = waehleZufaelligeMusik(gameMusikListe); // Verwendet gameMusikListe
-        File musicFile = new File("Musik/GameMusik/" + ausgewählteMusik);
-        speichereMusik(gameMusik, ausgewählteMusik);
-        
-        if (musicFile.exists()) {
-            // Wenn bereits ein MediaPlayer existiert, stoppen und zurücksetzen
-            if (mediaPlayer != null) {
-                mediaPlayer.stop();
-                mediaPlayer.dispose();  // MediaPlayer korrekt freigeben
-            }
-            
-            // Erstelle einen neuen MediaPlayer
-            Media media = new Media(musicFile.toURI().toString());
-            mediaPlayer = new MediaPlayer(media);
-            mediaPlayer.setVolume(this.getAktuelleLautstaerke());  // Setze die Lautstärke
-            mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);    // Musik in Schleife abspielen
-            mediaPlayer.play();
-            // System.out.println("Musikdatei " + musicFile.getName() + " wurde gespielt.");
-        } else {
-            System.out.println("Musikdatei " + musicFile.getName() + " wurde nicht gefunden.");
-        }
-    }
-
-    // Methode zum Stoppen der aktuellen Musik
-    public void stopMusik() {
+    // Musik stoppen
+    private void stoppeAktuelleMusik() {
         if (mediaPlayer != null) {
             mediaPlayer.stop();
-            mediaPlayer.dispose();  // MediaPlayer korrekt freigeben
-            mediaPlayer = null;  // Setze den MediaPlayer auf null, um ihn später ggf. neu zu erstellen
+            mediaPlayer.dispose();
+            mediaPlayer = null;
         }
     }
 
-    // Speichert die Musik
-    public void speichereMusik(String musikDatei, String ausgewählteMusik) {
-        String AktuelleMusik = musikDatei + ausgewählteMusik;
-        System.out.println(AktuelleMusik);
+    // Startet die Menü-Musik
+    public void startMenuMusik() {
+        stoppeAktuelleMusik();
+        
+        String ausgewählteMusik = waehleZufaelligeMusik(menuMusikListe);
+        File musicFile = new File("Musik/MenuMusik/" + ausgewählteMusik);
+        
+        if (musicFile.exists()) {
+            setUpMediaPlayer(musicFile);
+        } else {
+            System.out.println("Musikdatei " + musicFile.getName() + " wurde nicht gefunden.");
+        }
     }
 
-    // Wählt zufällig ein Musikstück aus der übergebenen Liste
+    // Startet die Spiel-Musik
+    public void startGameMusik() {
+        stoppeAktuelleMusik();
+        
+        String ausgewählteMusik = waehleZufaelligeMusik(gameMusikListe);
+        File musicFile = new File("Musik/GameMusik/" + ausgewählteMusik);
+        
+        if (musicFile.exists()) {
+            setUpMediaPlayer(musicFile);
+        } else {
+            System.out.println("Musikdatei " + musicFile.getName() + " wurde nicht gefunden.");
+        }
+    }
+
+    // Einrichten des MediaPlayers
+    private void setUpMediaPlayer(File musicFile) {
+        Media media = new Media(musicFile.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setVolume(this.getAktuelleLautstaerke());
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
+    }
+
+    // Wählt zufällig ein Musikstück aus der Liste
     private String waehleZufaelligeMusik(List<String> musikListe) {
         Random random = new Random();
         int index = random.nextInt(musikListe.size());
@@ -138,16 +109,16 @@ public class MusikPlayer {
         vbox.setPadding(new Insets(20));
 
         Label volumeLabel = new Label("Volume");
-        Slider volumeSlider = new Slider(0, 100, this.getAktuelleLautstaerke()); // Hole den aktuellen Lautstärkewert
-        volumeSlider.setBlockIncrement(0.1);
+        Slider volumeSlider = new Slider(0, 100, this.getAktuelleLautstaerke() * 100); // Lautstärkewert von 0 bis 100
+
+        volumeSlider.setBlockIncrement(1);
         volumeSlider.setShowTickLabels(true);
         volumeSlider.setShowTickMarks(true);
 
         volumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
-            this.setAktuelleLautstaerke(newValue.doubleValue()); // Aktualisiere die Lautstärke
+            this.setAktuelleLautstaerke(newValue.doubleValue() / 100); // Lautstärke von 0 bis 1
             if (mediaPlayer != null) {
-                mediaPlayer.setVolume(this.getAktuelleLautstaerke()); // Setze die Lautstärke im MediaPlayer
-                System.out.println("MusikSlider ändert die musik");
+                mediaPlayer.setVolume(this.getAktuelleLautstaerke());
             }
         });
 
